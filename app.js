b4df8f92e6617c1f815ee604812f1ea6d31f80ff
Shane Moe <shanekhantsoemoe98@gmail.com>
2020-12-20 05:16:31 -0500

    feat: cron auth tokens router added

--- app.js
+++ app.js
@@ -2,20 +2,18 @@ require('dotenv').config();
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var tokensRouter = require('./routes/tokens');
 
 var app = express();
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/tokens', tokensRouter);
 
 module.exports = app;
--- routes/tokens.js
+++ routes/tokens.js
@@ -3,7 +3,7 @@ var axios = require('axios');
 var router = express.Router();
 
 /* retrieves the code and setup the access_token and refresh_token */
-router.get('/', (req, res, next) => {
+router.get('/tokens', (req, res, next) => {
   const code = req.query.code;
   const url = `https://drchrono.com/o/authorize`;
   if (!code) {
@@ -15,11 +15,17 @@ router.get('/', (req, res, next) => {
     'redirect_uri': process.env.REDIRECT_URI,
     'client_id': process.env.CLIENT_ID,
     'client_secret': process.env.CLIENT_SECRETS,
-  }).then(function (response) {
-    console.log(response['access_token']);
-    console.log(response['refresh_token']);
-    console.log(response);
-  })
+  }).then((response) => {
+    res.status(201).json({
+      "Success": {
+        "access_token": response['access_token'],
+        "refresh_token": response['refresh_token'],
+        "expiration_seconds": response['expires_in']
+      }
+    });
+  }).catch((error) => {
+    res.status(404).json({ "Failed": error.message });
+  });
 });
 
 module.exports = router;
