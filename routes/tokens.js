d882c504153f2b65ac07e8904a72cae238a69503
Shane Moe <shanekhantsoemoe98@gmail.com>
2020-12-20 05:58:18 -0500

    refactor: removed header part in Axios ppost

--- routes/tokens.js
+++ routes/tokens.js
@@ -14,10 +14,6 @@ router.get('/', (req, res, next) => {
     'redirect_uri': process.env.REDIRECT_URI,
     'client_id': process.env.CLIENT_ID,
     'client_secret': process.env.CLIENT_SECRETS,
-  }, {
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    }
   }).then((response) => {
     console.log(response);
     res.status(201).json({
