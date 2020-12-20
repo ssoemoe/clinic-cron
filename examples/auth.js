637c9e51f1add827d855ae2508159c0c5baec38b
Shane Moe <shanekhantsoemoe98@gmail.com>
2020-12-20 04:50:57 -0500

    feat: examples added

---
+++ examples/auth.js
@@ -0,0 +1,5 @@
+const REDIRECT_URI = encodeURIComponent("https://clinic-cron.tk");
+const CLIENT_ID = encodeURIComponent("FXGdZVM66p1Bb3cJY5upbu19T77NIOsnOvSDJXch");
+// omitted to default to all scopes
+const url = `https://drchrono.com/o/authorize/?redirect_uri=${REDIRECT_URI}&response_type=code&client_id=${CLIENT_ID}`;
+console.log(url);
---
+++ examples/token.js
@@ -0,0 +1,14 @@
+const axios = require("axios");
+const code = 'Mm2eSE4EpQkyU0ZAsW7aA7R3VSotSv';
+const url = `https://drchrono.com/o/authorize`;
+axios.post(url, {
+    'code': code,
+    'grant_type': 'authorization_code',
+    'redirect_uri': 'https://clinic-cron.tk',
+    'client_id': 'FXGdZVM66p1Bb3cJY5upbu19T77NIOsnOvSDJXch',
+    'client_secret': 'jBLLxG6fOyxYIymXiWyPHDrTnaAuqALI1BH24KXxt3kY190umDl9cW5ZReCuvyXkOGDKC6Jvenrt0kvtCzGhVvLGqiOB4KbpWb2j6KPY4VftNyn59YFukwkoXsi4J9rD',
+}).then(function (response) {
+    console.log(response['access_token']);
+    console.log(response['refresh_token']);
+    console.log(response);
+})
