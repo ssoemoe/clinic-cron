c610c87077dd4ac9263b3a29fa130034ef53e04f
Shane Moe <shanekhantsoemoe98@gmail.com>
2020-12-31 01:29:55 -0500

    feat: responsive form layout structured

--- svc-cron/public/javascripts/index.js
+++ svc-cron/public/javascripts/index.js
@@ -1,17 +1,32 @@
 $(document).ready(function () {
-    let current = 0, max = 10;
-    const
-    const next = () => {
-        if (current === max)
-            return;
-        current++;
-        $('.new-patient-form').html('');
-        $('.new-patient-form').append();
+    let current = 0;
+    let params = {};
+    const divs = ['first', 'second', 'third'];
+    const url = window.location.href;
+    const keyValuePairs = url.split('?')[1].split('&');
+    for (let kv of keyValuePairs) {
+        let key = kv.split("=")[0].trim();
+        let value = kv.split("=")[1].trim();
+        params[key] = value;
+    }
+    const patientId = Number(params["id"]);
+
+    // init
+    $('#new-patient-form').html($(`#${divs[0]}`).html());
+    $('#prev').hide();
+
+    const slide = (direction) => {
+        if (direction === 'next' && current === divs.length - 1) return;
+        if (direction === 'prev' && current === 0) return;
+        current = direction === 'next' ? current + 1 : current - 1;
+        currentDiv = `#${divs[current]}`;
+        $('#new-patient-form').html($(currentDiv).html());
+        if (current !== 0) $('#prev').show();
+        else $('#prev').hide();
+        if (current === divs.length - 1) $('#next').hide();
+        else $('#next').show();
     }
 
-    const prev = () => {
-        if (current === 0)
-            return;
-        current--;
-    }
+    $('#next').on('click', function () { slide('next'); });
+    $('#prev').on('click', function () { slide('prev'); });
 });
