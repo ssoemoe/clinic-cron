dd9bf0d20d58ff1e872cf4faa013b486055a7353
Shane Moe <shanekhantsoemoe98@gmail.com>
2021-01-09 21:48:27 -0500

    variables: added more message intents

--- voice-end/src/app.js
+++ voice-end/src/app.js
@@ -41,15 +41,20 @@ app.setHandler({
     //get appointment data
     api.getAppointments().then(response => { console.log(response); this.$app.$data.appointmentData = response });
 
-    const welcomeMsg = ["Welcome to the Clinic Attendant!",
-      `${util.greetingTime()}! Welcome to the Clinic Attendant!`,
-      "Hello! Welcome to the Clinic Attendant!"
+    const welcomeMsg = ["Welcome to the clinic!",
+      "Welcome to our clinic!",
+      `${util.greetingTime()}! Welcome to the clinic!`,
+      "Hello! Welcome to our helpful clinic!",
+      "Hey! Welcome to the clinic",
     ]
-    const infoMsg = ["I am a doctor's smart assistant.",
-      "I an AI assistant built for doctors.",
-      "I an AI receptionist built for doctors.",
+    const infoMsg = ["I am a doctor's smart assistant and I will help you check-in your appointment. It is a pleasure to meet you.",
+      "I am a virutal assistant built for helping patients with appointment check-ins. It is nice to meet you.",
+      "I am a virtual receptionist specialized in helping people check-in with their appointments. It is good to meet you today.",
     ]
     const firstNameMgs = [
+      "Can you please give me your first name to start the appointment check-in?",
+      "Would you kindly tell me your first name for the appointment check-in?",
+      "Let's start the check-in. What is your first name?",
       "To check-in, please tell me your first name?",
       "To check-in, what is your first name?",
     ]
@@ -74,7 +79,6 @@ app.setHandler({
   },
 
   Help() {
-
     this.ask("Sorry, I didn't get that, please tell me your first name");
   },
 
